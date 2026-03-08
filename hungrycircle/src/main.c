9a44ed6209577212d150600fbbe0e5115a5a7a94
Josh Lawrence <josh@joshlawrence.dev>
2026-03-08 19:52:30 -0400

    feat: wrap actor across windows bounds

--- hungrycircle/src/main.c
+++ hungrycircle/src/main.c
@@ -4,6 +4,7 @@ const int FPS = 60;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 450;
 const char *TITLE = "raylib - hungrycircle";
+const float MOVEMENT_SPEED = 400.0f;
 
 typedef struct Actor {
   Vector2 position;
@@ -24,13 +25,13 @@ int main() {
       .heading = {1, 0},
       .size = 30.0f,
       .color = RED,
-      .speed = 200.0f,
+      .speed = MOVEMENT_SPEED,
   };
 
   while (!WindowShouldClose()) {
     float deltaTime = GetFrameTime();
     BeginDrawing();
-    ClearBackground(WHITE);
+    ClearBackground(BLACK);
     DrawFPS(10, 10);
     draw_actor(&actor, deltaTime);
     EndDrawing();
@@ -55,6 +56,19 @@ void draw_actor(Actor *actor, float deltaTime) {
   }
   if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
     newPosition.x += actor->speed * deltaTime;
+  }
+
+  if (newPosition.x < -actor->size) {
+    newPosition.x = GetScreenWidth() + actor->size;
+  }
+  if (newPosition.y < -actor->size) {
+    newPosition.y = GetScreenHeight() + actor->size;
+  }
+  if (newPosition.x > GetScreenWidth() + actor->size) {
+    newPosition.x = -actor->size;
+  }
+  if (newPosition.y > GetScreenHeight() + actor->size) {
+    newPosition.y = -actor->size;
   }
 
   actor->position = newPosition;
